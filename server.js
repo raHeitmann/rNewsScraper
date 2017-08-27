10a3977701494282cf44ad54422ac1951f31c9fe
Ryan Arnett <ryanglennarnett@gmail.com>
2017-08-26 21:24:12 -0500

    put listen back in

--- server.js
+++ server.js
@@ -204,6 +204,6 @@ app.get("/articles", function(req, res) {
 
 
 // Listen on port 3000
-// app.listen(3000, function() {
-//   console.log("App running on port 3000!");
-// });
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
